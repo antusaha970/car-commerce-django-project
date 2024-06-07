d3b62e393db50aa0ee48120e80ba1ea5bfe0ceac
Antu Saha <antusaha970@gmail.com>
2024-06-07 13:16:52 +0600

    authentication part completed

--- car/urls.py
+++ car/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.HomePage.as_view(), name="homePage"),
+    path('sign-up/', views.SignUpView.as_view(), name="sing_up"),
+    path('logout/', views.LogOutUserView.as_view(), name="logout"),
+    path('login/', views.LogInUserView.as_view(), name="login"),
+    path('update/', views.UpdateUserView.as_view(), name="update"),
+    path('profile/', views.ProfileView.as_view(), name="profile"),
     path('filter/<int:brand>/', views.HomePage.as_view(), name="brand_filter"),
 ]
--- car/views.py
+++ car/views.py
@@ -1,7 +1,68 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, TemplateView
+from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
 from .models import Car, Brand
+from .forms import SignUpForm, UpdateUserForm
 # Create your views here.
+
+
+class SignUpView(CreateView):
+    model = User
+    form_class = SignUpForm
+    success_url = reverse_lazy("homePage")
+    template_name = "authForm.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["type"] = 'Sign up'
+        return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password1"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(self.request, user=user)
+        return response
+
+
+class LogOutUserView(LogoutView):
+    next_page = reverse_lazy("homePage")
+
+
+class LogInUserView(LoginView):
+    success_url = reverse_lazy("homePage")
+    template_name = "authForm.html"
+    next_page = reverse_lazy("homePage")
+    redirect_authenticated_user = True
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["type"] = "Login"
+        return context
+
+
+class UpdateUserView(UpdateView):
+    model = User
+    form_class = UpdateUserForm
+    template_name = "authForm.html"
+    success_url = reverse_lazy("profile")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["type"] = 'Update'
+        return context
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class ProfileView(TemplateView):
+    template_name = "profile.html"
 
 
 class HomePage(ListView):
